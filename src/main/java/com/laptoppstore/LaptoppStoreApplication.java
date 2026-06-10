bafaf3f5b2e630efdc765bc438c7561ee66b1941
gamelalag <huu2342003@gmail.com>
2026-06-10 11:16:13 +0700

    feat: init project

---
+++ src/main/java/com/laptoppstore/LaptoppStoreApplication.java
@@ -0,0 +1,13 @@
+package com.laptoppstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptoppStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaptoppStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laptoppstore/LaptoppStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laptoppstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptoppStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
